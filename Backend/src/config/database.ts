02426cd4e32405788a59f2f24687e0b6b226ad03
Narendran S <122251349+SNarendranS@users.noreply.github.com>
2025-11-19 16:40:18 +0530

    configured database connection

---
+++ Backend/src/config/database.ts
@@ -0,0 +1,21 @@
+import { Sequelize } from 'sequelize';
+import dotenv from "dotenv";
+
+dotenv.config()
+
+export const sequelize = new Sequelize(
+  process.env.DB_URL as string,
+  {
+    dialect: 'postgres',
+    logging: false,
+  }
+);
+
+export const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connected successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/database";
 
 dotenv.config();
 
@@ -9,7 +10,10 @@ const port = process.env.PORT;
 app.use('/greet', (req, res) =>
     res.send("welcome to the inventory stock management")
 )
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+const start = async () => {
+    await connectDB()
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}
+start()
